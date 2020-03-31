c80cd8c633177bf058723e551c1e1c4e992731a0
RolandoJr29 <rolando_ar95@hotmail.com>
2020-03-30 19:35:35 -0500

    Búsqueda de nombres con AJAX

---
+++ Sem12/clase2-AJAX/a.js
@@ -0,0 +1,32 @@
+function funcion1(a){
+    return a + 2;
+}
+
+function funcion4(b){
+    return a + 2;
+}
+
+
+var a;
+a = factorial(30);
+
+var b;
+b = factorial(2 + a);
+
+
+
+
+
+
+
+function funcion1(){
+    return a + 2;
+}
+
+function funcion2(a){
+    return b;
+}
+
+function funcion3(b){
+    return c;
+}
