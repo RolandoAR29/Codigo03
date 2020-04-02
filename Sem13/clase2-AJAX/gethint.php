908cc18bd2252a5aa059f62543a8048787dd1815
RolandoJr29 <rolando_ar95@hotmail.com>
2020-04-01 19:59:15 -0500

    Login con AJAX

--- Sem13/clase2-AJAX/gethint.php
+++ Sem13/clase2-AJAX/gethint.php
@@ -1,62 +1,123 @@
 <?php
-// Array with names
-$a[] = "Anna";
-$a[] = "11sasASDx1a616x5as";
+// Arreglo de nombres y contraseñas de usuario
+$a[] = [
+    "nombre" => "Anna",
+    "pass" => "lb6vtISJgg=="
+];
+$a[] = [
+    "nombre" => "Pancha",
+    "pass" => "pLGvtru4gIR3"
+];
+$a[] = [
+    "nombre" => "María",
+    "pass" => "obGzFgC4gIR3"
+];
+$a[] = [
+    "nombre" => "Ricolás",
+    "pass" => "prmkwr8a8MV1kXc="
+];
+$a[] = [
+    "nombre" => "Roberto",
+    "pass" => "pr+juMXLvoN2kg=="
+];
+$a[] = [
+    "nombre" => "Juana",
+    "pass" => "nsWiwbSIgYU="
+];
+$a[] = [
+    "nombre" => "Doki",
+    "pass" => "mL+svISJgg=="
+];
+$a[] = [
+    "nombre" => "Nandito",
+    "pass" => "orGvt7zLvoN2kg=="
+];
 
+function encrypt($string, $key) {
+    $result = '';
+    for($i=0; $i<strlen($string); $i++) {
+       $char = substr($string, $i, 1);
+       $keychar = substr($key, ($i % strlen($key))-1, 1);
+       $char = chr(ord($char)+ord($keychar));
+       $result.=$char;
+    }
+    return base64_encode($result);
+}
 
-$a[] = "Cinderella";
-$a[] = "Diana";
-$a[] = "Eva";
-$a[] = "Fiona";
-$a[] = "Gunda";
-$a[] = "Hege";
-$a[] = "Inga";
-$a[] = "Johanna";
-$a[] = "Kitty";
-$a[] = "Linda";
-$a[] = "Nina";
-$a[] = "Ophelia";
-$a[] = "Petunia";
-$a[] = "Amanda";
-$a[] = "Raquel";
-$a[] = "Cindy";
-$a[] = "Doris";
-$a[] = "Eve";
-$a[] = "Evita";
-$a[] = "Sunniva";
-$a[] = "Tove";
-$a[] = "Unni";
-$a[] = "Violet";
-$a[] = "Liza";
-$a[] = "Elizabeth";
-$a[] = "Ellen";
-$a[] = "Wenche";
-$a[] = "Vicky";
+function decrypt($string, $key) {
+    $result = '';
+    $string = base64_decode($string);
+    for($i=0; $i<strlen($string); $i++) {
+       $char = substr($string, $i, 1);
+       $keychar = substr($key, ($i % strlen($key))-1, 1);
+       $char = chr(ord($char)-ord($keychar));
+       $result.=$char;
+    }
+    return $result;
+}
 
-// get the q parameter from URL
-$n = $_REQUEST["nombre"];
-$p = $_REQUEST["pass"];
+// Recibir los parámetros de la URL
+$nombre = $_REQUEST["nombre"];
+$pass = $_REQUEST["pass"];
 
-$p = desencriptar($p);
+//$claveCifrada = encrypt($pass, 'PASSWORD_DEFAULT');
+//$claveDescifrada = decrypt('lb6vtISJgg==', 'PASSWORD_DEFAULT');
 
-$hint = "";
-
-if ($q !== "") { // Validar que la cadena no esté vacía
-    $q = strtolower($q); // convertir a minúsculas // -->  $q = 'a'
-    $len = strlen($q); // lngitud de cadena // $len -> 1
-    foreach($a as $name) {
-        //stristr -> Compara dos cadenas y captura la primera aparición
-        if (stristr($q, substr($name, 0, $len))) {  // $name = 'Amanda' -> 'A' // 'a' == 'A'
-            //$name -> 'Anna' / 'Amanda'
-            if ($hint === "") {
-                $hint = $name;
+$bandera = false;
+if($nombre !== "" && $pass !== ""){
+    foreach($a as $name){
+        $nombreBd = strtolower($name['nombre']); //convertir a mínusculas el nombre sacado de la BD
+        $nombre = strtolower($nombre); //convertir a mínusculas el nombre ingresado desde el formulario
+        if($nombreBd == $nombre){
+            $claveCifrada = encrypt($pass, 'PASSWORD_DEFAULT');
+            if($name['pass'] == $claveCifrada){
+                echo "Clave correcto";
+            break;
             } else {
-                $hint .= ", $name";
+                echo "Contraseña incorrecta";
+            break;
             }
+        } else {
+            $bandera = true;
         }
     }
 }
 
-// Output "no suggestion" if no hint was found or output correct values
-echo $hint === "" ? "no ha sugerencias" : $hint;
+echo $bandera ? "Usuario no existe" : "";
+
+
+
+
+
+
+
+// $p = desencriptar($p);
+
+// $hint = "";
+
+// if ($q !== "") { // Validar que la cadena no esté vacía
+//     $q = strtolower($q); // convertir a minúsculas // -->  $q = 'a'
+//     $len = strlen($q); // lngitud de cadena // $len -> 1
+//     foreach($a as $name) {
+//         //stristr -> Compara dos cadenas y captura la primera aparición
+//         if (stristr($q, substr($name, 0, $len))) {  // $name = 'Amanda' -> 'A' // 'a' == 'A'
+//             //$name -> 'Anna' / 'Amanda'
+//             if ($hint === "") {
+//                 $hint = $name;
+//             } else {
+//                 $hint .= ", $name";
+//             }
+//         }
+//     }
+// }
+
+// // Output "no suggestion" if no hint was found or output correct values
+// echo $hint === "" ? "no ha sugerencias" : $hint;
+//echo "<script>alert('$claveCifrada')</script>";
 ?>
+<!-- <script language= javascript type= text/javascript >
+    MiFuncionJS()
+    {  alert (echo $claveCifrada)}
+</script> -->
+
+ 
--- Sem13/clase2-AJAX/script.js
+++ Sem13/clase2-AJAX/script.js
@@ -1,5 +1,28 @@
-function showHint(str) {
-    if (str.length == 0) {
+// function showHint(str) {
+//     if (str.length == 0) {
+//         document.getElementById("txtHint").innerHTML = "";
+//         return;
+//     } else {
+//         var xmlhttp = new XMLHttpRequest();
+//         xmlhttp.onreadystatechange = function() {
+//             if (this.readyState == 4 && this.status == 200) {
+//                 //lógica para mostrar repuesta
+//                 document.getElementById("txtHint").innerHTML = this.responseText;
+//             }
+//         };
+//         xmlhttp.open("GET", "gethint.php?q=" + str, true);
+//         xmlhttp.send();
+//     }
+// }
+
+function enviar(){
+    var nombre = document.getElementById('nombre').value;
+    var pass = document.getElementById('password').value;
+
+    var nombre = nombre.trim();
+    var pass = pass.trim();
+
+    if (nombre.length == 0 && pass.length == 0) {
         document.getElementById("txtHint").innerHTML = "";
         return;
     } else {
@@ -7,10 +30,11 @@ function showHint(str) {
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
                 //lógica para mostrar repuesta
-                //document.getElementById("txtHint").innerHTML = this.responseText;
+                document.getElementById("txtHint").innerHTML = this.responseText;
             }
         };
-        xmlhttp.open("GET", "gethint.php?q=" + str, true);
+        xmlhttp.open("GET", "gethint.php?nombre=" + nombre + "&pass=" + pass, true); //open(MÉTODO, URL, VALOR BOOLEANO QUE DETERMINA SI ES SÍNCRONO O ASÍNCRONO)
         xmlhttp.send();
     }
+
 }
