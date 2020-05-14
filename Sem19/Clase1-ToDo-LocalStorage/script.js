fe476e2b45acdb94f7b3428536ee22eaca46d9cf
RolandoJr29 <rolando_ar95@hotmail.com>
2020-05-13 20:22:02 -0500

    ToDo en LocalStorage - js, bootstrap

--- Sem19/Clase1-ToDo-LocalStorage/script.js
+++ Sem19/Clase1-ToDo-LocalStorage/script.js
@@ -26,22 +26,46 @@ const leerLocalStorage = () => {
         arrayTareas = [];
     } else {
         arrayTareas.forEach(element => {
-            listaTareas.innerHTML += `<div class="alert alert-danger" role="alert">
-                <span class="material-icons float-left mr-2">
-                    assignment
-                </span>
-                <b>${ element.tarea }</b> - ${ element.estado }
-                <span class="material-icons float-right">
-                    delete
-                </span>
-                <span class="material-icons float-right">
-                    done
-                </span>
-            </div>` 
+            if(element.estado){
+                listaTareas.innerHTML += `<div class="alert alert-success" role="alert">
+                    <span class="material-icons float-left mr-2">assignment</span>
+                    <b>${ element.tarea }</b> - ${ element.estado }
+                    <span class="material-icons float-right">delete</span>
+                    <span class="material-icons float-right">done</span>
+                </div>` 
+            } else {
+                listaTareas.innerHTML += `<div class="alert alert-danger" role="alert">
+                    <span class="material-icons float-left mr-2">assignment</span>
+                    <b>${ element.tarea }</b> - ${ element.estado }
+                    <span class="material-icons float-right">delete</span>
+                    <span class="material-icons float-right">done</span>
+                </div>`
+            }
         });
     }
 }
 
+const eliminarDeLocalStorage = (tarea) => {
+    let indiceArreglo;
+
+    arrayTareas.forEach((element, index) => {
+        if(element.tarea === tarea){
+            indiceArreglo = index;
+        }
+    });
+
+    arrayTareas.splice(indiceArreglo, 1);
+    guardarLocalStorage();
+}
+
+const editarDeLocalStorage = (tarea) => {
+    let indexArray = arrayTareas.findIndex((element) => element.tarea === tarea);
+    // (element) => {
+    //     return element.tarea === tarea
+    // }
+    arrayTareas[indexArray].estado = true;
+    guardarLocalStorage();
+}
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -57,6 +81,13 @@ document.addEventListener('DOMContentLoaded', leerLocalStorage);
 
 listaTareas.addEventListener('click', (e) => {
     e.preventDefault();
-
-    console.log(e);
+    if(e.target.innerHTML === 'done' || e.target.innerHTML === 'delete'){
+        let nombreTarea = e.path[1].childNodes[3].innerHTML;
+        if(e.target.innerHTML === 'done'){
+            editarDeLocalStorage(nombreTarea);
+        }
+        if(e.target.innerHTML === 'delete'){
+            eliminarDeLocalStorage(nombreTarea);
+        }
+    }
 })
