4e8a777bf0e97e41293105a9ca0119d70f573032
RolandoJr29 <rolando_ar95@hotmail.com>
2020-06-16 19:01:05 -0500

    Formularios en laravel

---
+++ Sem24/Clase1-FormulariosAngular/formularios/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TemplateComponent } from './components/template/template.component';
+import { ReactiveComponent } from './components/reactive/reactive.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TemplateComponent,
+    ReactiveComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
