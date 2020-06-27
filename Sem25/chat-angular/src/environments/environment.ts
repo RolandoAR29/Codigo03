9ca499fcb4b0b90a71043026dc3dce77bbb17174
RolandoJr29 <rolando_ar95@hotmail.com>
2020-06-26 20:28:44 -0500

    Chat con angular - firebase

---
+++ Sem25/chat-angular/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAtH6zi-N0hsFBR0kqvRD8snSV9EjAGnIk",
+    authDomain: "chat-angular-a4124.firebaseapp.com",
+    databaseURL: "https://chat-angular-a4124.firebaseio.com",
+    projectId: "chat-angular-a4124",
+    storageBucket: "chat-angular-a4124.appspot.com",
+    messagingSenderId: "391410792300",
+    appId: "1:391410792300:web:bd52559aac2b1997f02549"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
--- Sem25/readme.md
+++ Sem25/readme.md
@@ -1,1 +1,5 @@
 ng g m appRouting       -> Crear module para rutas
+
+
+#Chat-Angular
+npm install angularfire2 firebase --save        ->  Instalar angularfire2
