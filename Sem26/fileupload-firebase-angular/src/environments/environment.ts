b426e71986ed419dec702a8befa0013aaf2931a1
RolandoJr29 <rolando_ar95@hotmail.com>
2020-07-02 20:04:00 -0500

    Maquetado de carga de imagenes a Firebase

---
+++ Sem26/fileupload-firebase-angular/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAA0Aef6QV9nczIDY47MkeKt5fypVQm8tc",
+    authDomain: "imagenes-angular-594f0.firebaseapp.com",
+    databaseURL: "https://imagenes-angular-594f0.firebaseio.com",
+    projectId: "imagenes-angular-594f0",
+    storageBucket: "imagenes-angular-594f0.appspot.com",
+    messagingSenderId: "364107345559",
+    appId: "1:364107345559:web:e2369d3d0e2c714f7fc3d2"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
