5491cb41d523c676f1a41690d296d1a45690df76
RolandoJr29 <rolando_ar95@hotmail.com>
2020-07-13 21:54:47 -0500

    Primera aplicación en REACT.JS

---
+++ Sem29/my-first-app/src/PrimeraApp.js
@@ -0,0 +1,43 @@
+// import React from 'react';
+// import React, { Fragment } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PrimeraApp = ({ saludo, subtitulo }) => {
+    // return (
+    //     <div>
+    //         <h1>Hola mundo</h1>
+    //         <p>Bienvenido</p>
+    //     </div>
+    // );
+
+    // return (
+    //     <Fragment>
+    //         <h1>Hola mundo</h1>
+    //         <p>Bienvenido</p>
+    //     </Fragment>
+    // );
+    console.log(saludo);
+
+    // if(!saludo){
+    //     throw new Error('El saludo es necesario');
+    // }
+
+    return (
+        <>
+            <h1> { saludo } </h1>
+            {/* <pre> { JSON.stringify(saludo,null, 3) } </pre> */}
+            <p>{ subtitulo }</p>
+        </>
+    );
+}
+
+PrimeraApp.propTypes = {
+    saludo: PropTypes.string.isRequired
+}
+
+PrimeraApp.defaultProps = {
+    subtitulo: 'Soy un subtitulo',
+}
+
+export default PrimeraApp;
---
+++ Sem29/my-first-app/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import PrimeraApp from './PrimeraApp';
+
+import './index.css';
+import CounterApp from './CounterApp';
+
+// const saludo = <h1>Hola mundo</h1>;
+
+const divRoot = document.querySelector('#root');
+
+// document.body.append(saludo);
+
+// ReactDOM.render( <PrimeraApp saludo="Hola, soy Goku"/> , divRoot);
+
+ReactDOM.render( <CounterApp value={ 10 }/> , divRoot);
